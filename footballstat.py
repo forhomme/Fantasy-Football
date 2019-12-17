000e6006eca3de02d41e1cfb8b15dc43547b7771
Ramadhoni <moehammad.ramadhoni@mobik-solutions.com>
2019-12-17 13:57:38 +0700

    edit selenium

--- footballstat.py
+++ footballstat.py
@@ -1,39 +1,158 @@
 import pandas as pd
 import numpy as np
+import re
+import random
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+import datetime
+from xvfbwrapper import Xvfb
+from matplotlib import pyplot as plt
+import seaborn as sns
 
-#read csv data
-xg_data = pd.read_csv('epl_xg.csv')
+# start xvfb
+display = Xvfb()
+display.start()
+
+# get from url
+xg_url = 'https://understat.com/league/EPL'
+# xg_req = requests.get(xg_url)
+# xg_html = xg_req.content
+
+# selenium to access data
+options = webdriver.FirefoxOptions()
+options.add_argument('headless')
+driver = webdriver.Firefox(firefox_options=options)
+driver.implicitly_wait(10)
+driver.get(xg_url)
+soup = BeautifulSoup(driver.page_source, 'lxml')
+
+# access element from inspect element
+headers = soup.find('div', attrs={'class': 'chemp margin-top jTable'}).find('table').find_all('th',attrs={'class':'sort'})
+
+# iterate over header
+headers_list = []
+for header in headers:
+    headers_list.append(header.get_text(strip=True))
+
+# call elemet in the header
+body = soup.find('div', attrs={'class': 'chemp margin-top jTable'}).table.tbody
+all_row_list = []
+for tr in body.find_all('tr'):
+    row = tr.find_all('td')
+    current_row = []
+    for item in row:
+        current_row.append(item.get_text(strip=True))
+    all_row_list.append(current_row)
+
+xg_df = pd.DataFrame(all_row_list, columns=headers_list)
+
+# get rid unwanted text
+xg_df['xG'] = xg_df['xG'].str.extract(r'^(.+?)[\+\-]', expand=True)
+xg_df['xGA'] = xg_df['xGA'].str.extract(r'^(.+?)[\+\-]', expand=True)
+xg_df['xPTS'] = xg_df['xPTS'].str.extract(r'^(.+?)[\+\-]', expand=True)
+xg_data = xg_df[['Team', 'M', 'W', 'L', 'G', 'GA', 'PTS', 'xG', 'xGA', 'xPTS']]
+xg_data = xg_data.replace('Wolverhampton Wanderers', 'Wolverhampton')
+driver.close()
 
 # add new goal difference columns
+pd.set_option('mode.chained_assignment', None)
+cols = ['M', 'W', 'L', 'G', 'GA', 'PTS', 'xG', 'xGA', 'xPTS']
+for col in cols:
+    xg_data[col] = pd.to_numeric(xg_data[col])
 xg_data['GD'] = xg_data['G'] - xg_data['GA']
 xg_data['xGD'] = xg_data['xG'] - xg_data['xGA']
-xg_data['NPxGD'] = xg_data['NPxG'] - xg_data['NPxGA']
 
 # plot real vs expected
 xg_data['GD_vs_xGD'] = xg_data['GD'] - xg_data['xGD']
 xg_data = xg_data.sort_values(by=['GD_vs_xGD'], ascending=False)
 
-cols = ['G', 'GA', 'xG', 'xGA', 'NPxG', 'NPxGA', 'GD', 'xGD', 'NPxGD']
+# Set the plot style and colour palette to use (remember dodgy spelling if you're from the UK!)
+sns.set(style='whitegrid')
+sns.set_color_codes('muted')
+
+# Initialize the matplotlib figure (f) and axes (ax), and set width and height of the plot
+f, ax = plt.subplots(figsize=(12, 10))
+
+# Create the plot, choosing the variables for each axis, the data source and the colour (b = blue)
+sns.barplot(x='GD_vs_xGD', y='Team', data=xg_data, color='b')
+
+# Rename the axes, setting y axis label to be blank
+ax.set(ylabel='', xlabel='Goal Difference vs Expected Goal Difference')
+
+# Remove the borders from the plot
+sns.despine(left=True, bottom=True)
+plt.show()
+
+cols = ['G', 'GA', 'xG', 'xGA', 'GD', 'xGD']
 for col in cols:
-    xg_data['%s_pg' % col] = xg_data[col] / xg_data['Games']
+    xg_data['%s_pg' % col] = xg_data[col] / xg_data['M']
 
-#load next fixture
-fixtures = pd.read_csv('epl_fixtures.csv')
+# load next fixture
+# start xvfb
+# display = Xvfb()
+# display.start()
 
+# get from url
+fixture_url = 'https://us.soccerway.com/national/england/premier-league/20192020/regular-season/r53145/'
+# fixture_req = requests.get(fixture_url)
+# fixture_html = fixture_req.content
+
+# selenium to access data
+# options = webdriver.FirefoxOptions()
+# options.add_argument('headless')
+# driver = webdriver.Firefox(firefox_options=options)
+driver.implicitly_wait(15)
+driver.get(fixture_url)
+soup = BeautifulSoup(driver.page_source, 'lxml')
+
+# access element from inspect element
+headers = soup.find('div', attrs={
+    'class': 'table-container'}).find('table').find_all('th')
+
+# iterate over header
+headers_list = []
+for header in headers:
+    headers_list.append(header.get_text(strip=True))
+
+if len(headers_list) < 7:
+    headers_list.append('')
+
+# call element in the header
+body = soup.find('div', attrs={'class': 'table-container'}).table.tbody
+all_row_list = []
+for tr in body.find_all('tr'):
+    row = tr.find_all('td')
+    current_row = []
+    for item in row:
+        current_row.append(item.get_text(strip=True))
+    all_row_list.append(current_row)
+
+fixture_df = pd.DataFrame(all_row_list, columns=headers_list)
+fixture_df = fixture_df.rename(columns={'Home team': 'Home_Team', 'Away team': 'Away_Team'})
+fixture_df = fixture_df[['Date', 'Home_Team', 'Away_Team']]
+fixture_df = fixture_df.replace(['Newcastle United', 'AFC Bournemouth', 'Leicester City', 'Norwich City', 'West Ham United',
+                       'Tottenham Hotspur', 'Wolverhampton …','Brighton & Hov…'],
+                      ['Newcastle', 'Bournemouth', 'Leicester', 'Norwich', 'West Ham', 'Tottenham', 'Wolverhampton', 'Brighton'])
+driver.close()
+display.stop()
+
+# calculate over fixture
 xg_data_pg = xg_data[['Team', 'xG_pg', 'xGA_pg']]
-fixtures = pd.merge(fixtures, xg_data_pg, left_on='Home_Team', right_on='Team')
-fixtures = pd.merge(fixtures, xg_data_pg, left_on='Away_Team', right_on='Team')
-fixtures = fixtures.drop(['Team_x', 'Team_y'], axis=1)
-fixtures = fixtures.rename(columns={
+fixture_df = pd.merge(fixture_df, xg_data_pg, left_on='Home_Team', right_on='Team')
+fixture_df = pd.merge(fixture_df, xg_data_pg, left_on='Away_Team', right_on='Team')
+fixture_df = fixture_df.drop(['Team_x', 'Team_y'], axis=1)
+fixture_df = fixture_df.rename(columns={
     'xG_pg_x': 'xG_pg_home', 'xGA_pg_x': 'xGA_pg_home', 'xG_pg_y': 'xG_pg_away', 'xGA_pg_y': 'xGA_pg_away'})
 
-#calculated adjusted stat
+# calculated adjusted stat
 xG_avg = xg_data_pg['xG_pg'].mean()
-matchups = fixtures.copy()
+matchups = fixture_df.copy()
 matchups['xG_adjusted_home'] = (matchups['xG_pg_home'] * matchups['xGA_pg_away']) / xG_avg
 matchups['xG_adjusted_away'] = (matchups['xG_pg_away'] * matchups['xGA_pg_home']) / xG_avg
 
-#calculate home advantage
+# calculate home advantage
 matchups['xG_adjusted_home'] = matchups['xG_adjusted_home'] * 1.1
 matchups['xG_adjusted_away'] = matchups['xG_adjusted_away'] * 0.87
 
